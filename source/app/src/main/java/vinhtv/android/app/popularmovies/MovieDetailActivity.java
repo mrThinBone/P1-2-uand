b0f51071e5235542146c8f24c37ddb40285b895d
mrThinBone <vinh.tv2003@gmail.com>
2017-03-28 15:24:43 +0700

    complete movie details, trailer videos & reviews

--- source/app/src/main/java/vinhtv/android/app/popularmovies/MovieDetailActivity.java
+++ source/app/src/main/java/vinhtv/android/app/popularmovies/MovieDetailActivity.java
@@ -1,27 +1,32 @@
 package vinhtv.android.app.popularmovies;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import vinhtv.android.app.popularmovies.data.Movie;
 import vinhtv.android.app.popularmovies.utilities.AddFavoriteTask;
 import vinhtv.android.app.popularmovies.utilities.AddFavoriteTask.TaskResult;
 import vinhtv.android.app.popularmovies.utilities.FetchMovieStuffTask;
 import vinhtv.android.app.popularmovies.utilities.FetchMovieStuffTask.MovieStuff;
-import vinhtv.android.app.popularmovies.utilities.FetchMovieStuffTask.Video;
-import vinhtv.android.app.popularmovies.utilities.FetchMovieStuffTask.Review;
 import vinhtv.android.app.popularmovies.utilities.NetworkUtils;
-
-import static vinhtv.android.app.popularmovies.R.id.detail_movie_releaseDate;
 
 public class MovieDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<TaskResult>,
         View.OnClickListener {
@@ -30,30 +35,25 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
     private static final int MOVIE_STUFF_LOADER_ID = 1002;
     static final String EXTRA_DATA = "ext_data";
 
-    TextView tvMovieTitle;
-    ImageView ivMoviePoster;
-    TextView tvMovieRating;
-    TextView tvMovieReleaseDate;
-    TextView tvMovieOverview;
-    TextView tvMovieDuration;
-    Button btnAddFavorite;
+    @BindView(R.id.detail_movie_title) TextView tvMovieTitle;
+    @BindView(R.id.detail_movie_poster) ImageView ivMoviePoster;
+    @BindView(R.id.detail_movie_rating) TextView tvMovieRating;
+    @BindView(R.id.detail_movie_releaseDate) TextView tvMovieReleaseDate;
+    @BindView(R.id.detail_movie_overview) TextView tvMovieOverview;
+    @BindView(R.id.detail_movie_duration) TextView tvMovieDuration;
+    @BindView(R.id.ll_movie_reviews) LinearLayout mReviewsContainer;
+    @BindView(R.id.ll_movie_videos) LinearLayout mVideosContainer;
 
     Toast mResultMsg;
     private Movie mMovie;
+    private boolean loaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        tvMovieTitle = (TextView) findViewById(R.id.detail_movie_title);
-        ivMoviePoster = (ImageView) findViewById(R.id.detail_movie_poster);
-        tvMovieRating = (TextView) findViewById(R.id.detail_movie_rating);
-        tvMovieReleaseDate = (TextView) findViewById(detail_movie_releaseDate);
-        tvMovieOverview = (TextView) findViewById(R.id.detail_movie_overview);
-        tvMovieDuration = (TextView) findViewById(R.id.detail_movie_duration);
-        btnAddFavorite = (Button) findViewById(R.id.btn_add_favorite);
+        ButterKnife.bind(this);
 
-        btnAddFavorite.setOnClickListener(this);
         Movie movie = getIntent().getParcelableExtra(EXTRA_DATA);
         tvMovieTitle.setText(movie.title());
         tvMovieReleaseDate.setText(movie.releaseDate());
@@ -70,11 +70,26 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
         getSupportLoaderManager().initLoader(MOVIE_STUFF_LOADER_ID, args, movieStuffLoaderCallbacks);
     }
 
-    @Override
-    public void onClick(View view) {
+    @OnClick(R.id.btn_add_favorite)
+    public void addToFavoriteCollection() {
         Bundle args = new Bundle();
         args.putParcelable(AddFavoriteTask.ARG_DATA, mMovie);
         getSupportLoaderManager().restartLoader(ADD_REMOVE_FAVORITE_LOADER_ID, args, this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String key = (String) view.getTag();
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + key));
+
+        // Check if the youtube app exists on the device
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            // If the youtube app doesn't exist, then use the browser
+            intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.youtube.com/watch?v=" + key));
+        }
+
+        startActivity(intent);
     }
 
     @Override
@@ -109,7 +124,50 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
 
         @Override
         public void onLoadFinished(Loader<MovieStuff> loader, MovieStuff data) {
-            data.toString();
+            if(data == null || loaded) return;
+
+            LayoutInflater layoutInflater = LayoutInflater.from(MovieDetailActivity.this);
+            if(data.videos.length > 0) {
+                mVideosContainer.setVisibility(View.VISIBLE);
+                int length = data.videos.length;
+                for (int i=0; i<length; i++) {
+                    FetchMovieStuffTask.Video video = data.videos[i];
+
+                    ViewGroup videoLayout = (ViewGroup) layoutInflater.inflate(R.layout.item_movie_video, null, false);
+                    TextView tvVideoTitle = (TextView) videoLayout.findViewById(R.id.tv_trailer_title);
+                    ImageButton btnPlay = (ImageButton) videoLayout.findViewById(R.id.btn_trailer_play);
+                    tvVideoTitle.setText(video.name);
+                    btnPlay.setTag(video.key);
+                    btnPlay.setOnClickListener(MovieDetailActivity.this);
+
+                    mVideosContainer.addView(videoLayout);
+                    if(i != length-1) {
+                        View separator = layoutInflater.inflate(R.layout.view_separator, null, false);
+                        mVideosContainer.addView(separator);
+                    }
+                }
+            }
+
+            if(data.reviews.length > 0) {
+                Resources resources = getResources();
+                mReviewsContainer.setVisibility(View.VISIBLE);
+                int length = data.reviews.length;
+                for (int i=0; i<length; i++) {
+                    FetchMovieStuffTask.Review review = data.reviews[i];
+
+                    ViewGroup reviewLayout = (ViewGroup) layoutInflater.inflate(R.layout.item_movie_review, null, false);
+                    TextView tvAuthor = (TextView) reviewLayout.findViewById(R.id.tv_review_author);
+                    TextView tvContent = (TextView) reviewLayout.findViewById(R.id.tv_review_content);
+                    tvAuthor.setText(review.author);
+                    tvContent.setText(review.content);
+                    if(i % 2 != 0) {
+                        reviewLayout.setBackgroundColor(resources.getColor(R.color.colorLowAccent));
+                    }
+
+                    mReviewsContainer.addView(reviewLayout);
+                }
+            }
+            loaded = true;
         }
 
         @Override
